08977bccd257fee1ca6ef6397aeb73734c258526
mustafa276 <mmustafamajeed1@gmail.com>
2024-08-27 12:31:47 +0500

    odd=even

---
+++ activity2.py
@@ -0,0 +1,17 @@
+a = 10
+b = 12
+c = 12
+print(a != b)
+print(b != c)
+a = "python"
+b = "coding"
+print(a, " and ", b, " are not the same")
+a = 4
+b = 5
+if (a==1) != (b==5):
+    print("hello")
+    a = int(input("enter a number"))
+    if a%2 != 0:
+        print(a, " is an odd number")
+    else:
+        print(a, " is an even number")
