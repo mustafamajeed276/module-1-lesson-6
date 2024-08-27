ce520865c4d7790de3642b2a874d4121ac79dfd1
mustafa276 <mmustafamajeed1@gmail.com>
2024-08-27 12:20:04 +0500

    OR-AND

---
+++ activity1.py
@@ -0,0 +1,22 @@
+a = 10
+b = 12
+c = 0
+if a and b and c:
+    print("all the numbers have boolean value as true")
+else:
+    print("at least one of the numbers have boolean value as false")
+
+
+
+    a = 10
+    b = -10
+    c = 0
+    if a > 0 or b > 0:
+        print("a or b is a positive number")
+    else:
+        print("no number is positive")
+
+        if b > 0 or c > 0:
+            print("b or c is a positive number")
+        else:
+            print("no number is positive")
